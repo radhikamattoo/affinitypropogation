d1e6eece907e76062443785f700d3118e3a97dcc
Radhika Mattoo <rm3485@nyu.edu>
2017-08-07 11:24:06 -0400

    skeleton files

---
+++ affinity_propogation.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+==================================================
+Affinity Propogation for Improved Volume Rendering
+==================================================
+@author: Radhika Mattoo, http://www.github.com/radhikamattoo
+@organization: MediVis, Inc.
+@contact: rm3485@nyu.edu
+
+Reference:
+Tianjin Zhang et al., "A Clustering-Based Automatic Transfer Function Design for
+Volume Visualization", Hindawi Sept. 2016
+"""
+print(__doc__)
+from sklearn.cluster import AffinityPropagation
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+##############################################################################
+# Data Collection & Preprocessing
+##############################################################################
+
+
+##############################################################################
+# IGM Construction & Refinement
+##############################################################################
+
+
+##############################################################################
+# Affinity Propogation
+##############################################################################
+
+
+
+##############################################################################
+# Transfer Function Generation
+##############################################################################
+
+
+
+##############################################################################
+# Volume Rendering
+##############################################################################
