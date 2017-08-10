f9c8393245045b394596842c855ecbe47fcf7ac2
Radhika Mattoo <rm3485@nyu.edu>
2017-08-10 15:20:55 -0400

    constructed basic IGM histogram, but needs refinement

--- affinity_propogation.py
+++ affinity_propogation.py
@@ -12,19 +12,63 @@ Tianjin Zhang et al., "A Clustering-Based Automatic Transfer Function Design for
 Volume Visualization", Hindawi Sept. 2016
 """
 print(__doc__)
-from sklearn.cluster import AffinityPropagation
-from sklearn import metrics
-from sklearn.datasets.samples_generator import make_blobs
+import os
+import sys
+import numpy as np
 import matplotlib.pyplot as plt
-from itertools import cycle
+import medpy
+import scipy
+from scipy import ndimage
+from medpy.io import load
+from medpy.features.intensity import intensities
 
 ##############################################################################
 # Data Collection & Preprocessing
 ##############################################################################
+data_path = "data/CR-MONO1-10-chest"
+image_data, image_header = load(data_path)
+image_data = np.rot90(image_data,k=3)
+print image_data.shape, image_data.dtype
+intensity = intensities(image_data)
+# print intensities(image_data)
+
+# Calculate gradient magnitude and angle using Sobel filters
+gx = ndimage.sobel(image_data, axis=0, mode='constant')
+gy = ndimage.sobel(image_data, axis=1, mode='constant')
+mag = np.sqrt(np.square(gx) + np.square(gy))
+angle = np.arctan(gy/gx)
+
+# Plot gradient
+# plt.close("all")
+# plt.figure()
+# plt.suptitle("Image, and it gradient along each axis")
+# ax = plt.subplot("131")
+# ax.axis("off")
+# ax.imshow(mag)
+# ax.set_title("magnitude")
+#
+# ax = plt.subplot("132")
+# ax.axis("off")
+# ax.imshow(gx)
+# ax.set_title("gx")
+#
+# ax = plt.subplot("133")
+# ax.axis("off")
+# ax.imshow(gy)
+# ax.set_title("gy")
+# plt.show()
+
+# Construct IGM
+plt.scatter(intensity, mag, alpha=0.006)
+plt.show()
+
+##############################################################################
+# Variance
+##############################################################################
 
 
 ##############################################################################
-# IGM Construction & Refinement
+# Refinement through removal of noisy 'bins'
 ##############################################################################
 
 
